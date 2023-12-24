d7ce7743791e86e4d5213c2c6226501939765ab3
yebology <yobelnathaniel12@gmail.com>
2023-12-24 23:49:20 +0700

    ketinggalan

---
+++ app/src/main/java/com/example/alp_vp/navigation/Navigation.kt
@@ -0,0 +1,50 @@
+package com.example.alp_vp.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.alp_vp.ui.view.authentication.AuthenticationView
+import com.example.alp_vp.ui.view.authentication.SignInView
+import com.example.alp_vp.ui.view.authentication.SignUpView
+import com.example.alp_vp.ui.view.authentication.SplashScreenView
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun Navigation(
+    navController: NavHostController
+) {
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+
+        composable(
+            route = Screen.Splash.route
+        ) {
+            SplashScreenView(navController = navController)
+        }
+
+        composable(
+            route = Screen.Authentication.route
+        ) {
+            AuthenticationView(navController = navController)
+        }
+
+        composable(
+            route = Screen.SignIn.route
+        ) {
+            SignInView()
+        }
+
+        composable(
+            route = Screen.SignUp.route
+        ) {
+            SignUpView()
+        }
+
+    }
+}
