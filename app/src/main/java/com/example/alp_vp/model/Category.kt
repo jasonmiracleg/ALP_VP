245dc99d9c8d6ba9a023d4f80eff293e88439c64
jasonmiracleg <jasonmiracle15@gmail.com>
2024-01-05 20:05:03 +0700

    Solving Merge Conflict

--- app/src/main/java/com/example/alp_vp/model/Category.kt
+++ app/src/main/java/com/example/alp_vp/model/Category.kt
@@ -1,13 +1,6 @@
 package com.example.alp_vp.model
 
-<<<<<<< HEAD
-class Category (
-    var title:  String,
-    var color:  String
-)
-=======
 data class Category(
     val category_title: String,
     val color: String
 )
->>>>>>> Jason
