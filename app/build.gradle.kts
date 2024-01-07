98fd68c9a346cee8177f1a8cea25280af702cf63
jasonmiracleg <jasonmiracle15@gmail.com>
2024-01-07 21:18:14 +0700

    Finalize Gradle (I Hope)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,12 @@ dependencies {
     implementation("com.google.accompanist:accompanist-permissions:0.30.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
     implementation("com.github.skydoves:colorpicker-compose:1.0.7") // Color Picker
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.4.0") // library compose that use synchronize
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // add library retrofit converter gson
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1") // add library to recognize json
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0") // add library to convert the json
+    implementation("com.squareup.okhttp3:okhttp:4.11.0") // add library REST function
+    implementation("androidx.datastore:datastore-preferences:1.0.0") // store token
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
